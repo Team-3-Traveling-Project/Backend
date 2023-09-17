995900b1495f1bdf0d8f2687e49554eaad30d1d2
김정환 <rocky990903@naver.com>
2023-09-17 18:52:34 +0900

    엔티티

--- src/main/java/com/sparta/travel/domain/entity/Place.java
+++ src/main/java/com/sparta/travel/domain/entity/Place.java
@@ -1,4 +1,40 @@
 package com.sparta.travel.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "place")
+@Getter
+@NoArgsConstructor
+
 public class Place {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    @Column(name = "placeName",nullable = false)
+    private String placeName;
+
+    @Column(name = "addressName", nullable = false)
+    private String addressName;
+
+    @Column(name = "roadAddressName", nullable = false)
+    private String roadAddressName;
+
+    @Column(name = "x", nullable = false)
+    private String x;
+
+    @Column(name = "y", nullable = false)
+    private String y;
+
+    @Column(name = "img_url", nullable = false)
+    private String img_url;
+
+    @ManyToOne
+    @JoinColumn(name = "plan_id")
+    private Plan plan;
+
+
 }
--- src/main/java/com/sparta/travel/domain/entity/Plan.java
+++ src/main/java/com/sparta/travel/domain/entity/Plan.java
@@ -1,4 +1,35 @@
 package com.sparta.travel.domain.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Table(name = "plan")
+@Getter
+@NoArgsConstructor
 public class Plan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Column(name ="city", nullable = false)
+    private String city;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "plan", cascade = {CascadeType.PERSIST})
+    private List<Place> placeList = new ArrayList<>();
 }
