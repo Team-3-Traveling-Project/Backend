3a5236dd747f17e4e27cab79ad2dac57de0a3470
whitehighdragon <47877063+SangYoungBaek@users.noreply.github.com>
2023-09-17 18:40:15 +0900

    Update Bookmark.java

--- src/main/java/com/sparta/travel/domain/entity/Bookmark.java
+++ src/main/java/com/sparta/travel/domain/entity/Bookmark.java
@@ -1,4 +1,37 @@
 package com.sparta.travel.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "bookmark")
 public class Bookmark {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String placeName;
+
+    @Column(nullable = false)
+    private String addressName;
+
+    @Column(nullable = false)
+    private String roadName;
+
+    @Column(nullable = false)
+    private String x;
+
+    @Column(nullable = false)
+    private String y;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
 }
